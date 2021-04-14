99c86cb0f307491362007e6b66f55c40425f048c
Jamie Carter <jc2820@hotmail.com>
2021-04-14 19:11:31 +0100

    lalalalla

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
 import Layout from "../components/layout";
+import Image from "next/image"
 import { attributes } from "../content/home.md";
 
+
 export default function Home() {
-  let { title, cats } = attributes;
+  let { HomeImage } = attributes;
   return (
     <Layout>
       <article>
-        <h1>{title}</h1>
-        
+        <h1>heyo</h1>
+        <Image
+        src={"/" + HomeImage}
+        width={300}
+        height={100}
+        alt={"fuck u m8"} />
       </article>
     </Layout>
   );
