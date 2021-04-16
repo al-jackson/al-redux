b77e881513d03b8c5e5530f303c2bc608edd100c
Jamie Carter <jc2820@hotmail.com>
2021-04-16 17:34:54 +0100

    scrolly scrool

--- pages/ode.js
+++ pages/ode.js
@@ -1,20 +1,14 @@
 import Layout from "../components/layout";
 import styles from "../styles/ode.module.css";
 import { attributes } from "../content/ode.md";
-import ReactMarkdown from "react-markdown"
 
 export default function Ode() {
-  let { odePost } = attributes;
+  //let { odePost } = attributes;
+  console.log(attributes);
   return (
     <Layout page={"Ode"}>
-      <div className={styles.background}>
-        {
-          odePost.map((ode) => {
-            //console.log(ode.text)
-            <ReactMarkdown source={ode.text} />
-          })
-        }
-      </div>
+      <div className={styles.background}></div>
+    
     </Layout>
   );
 }
