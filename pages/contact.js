44fea4ce8ccd7558c0c07a6f5a1baaa9549279f8
Jamie Carter <jc2820@hotmail.com>
2021-04-16 19:42:52 +0100

    better style for contact page

--- pages/contact.js
+++ pages/contact.js
@@ -6,15 +6,33 @@ export default function Contact() {
     <Layout page={"Contact"}>
       <div className={styles.background}></div>
       <section className={styles.contactContainer}>
-        <div className={styles.contactType}>
-          <img src="/img/gmail-icon.png" alt="gmail icon" className={styles.gmailIcon} />
-          <p>alpauljackson@gmail.com</p>
+        <div className={styles.contactAligner}>
+          <div className={styles.contactType}>
+            <div className={styles.leftColumn}>
+              <img
+                src="/img/gmail-icon.png"
+                alt="gmail icon"
+                className={styles.gmailIcon}
+              />
+            </div>
+            <div className={styles.rightColumn}>
+              <p>alpauljackson@gmail.com</p>
+            </div>
+          </div>
         </div>
         <div className={styles.contactType}>
-        <img src="/img/insta-icon.png" alt="instagram icon" className={styles.instaIcon} />
-          <a href="https://instagram.com/al_redux/" alt="al's insta">
-            al_redux
-          </a>
+          <div className={styles.leftColumn}>
+            <img
+              src="/img/insta-icon.png"
+              alt="instagram icon"
+              className={styles.instaIcon}
+            />
+          </div>
+          <div className={styles.rightColumn}>
+            <a href="https://instagram.com/al_redux/" alt="al's insta">
+              al_redux
+            </a>
+          </div>
         </div>
       </section>
     </Layout>
