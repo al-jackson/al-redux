9c4437d03b04cd929fc77b169406b5cc88d9b69b
Jamie Carter <jc2820@hotmail.com>
2021-04-16 18:36:58 +0100

    remove shite packages

--- pages/ode.js
+++ pages/ode.js
@@ -1,14 +1,14 @@
 import Layout from "../components/layout";
 import styles from "../styles/ode.module.css";
-import { attributes } from "../content/ode.md";
+import { react as OdeContent } from "../content/ode.md";
 
 export default function Ode() {
-  //let { odePost } = attributes;
-  console.log(attributes);
   return (
     <Layout page={"Ode"}>
       <div className={styles.background}></div>
-    
+      <section className={styles.odeContainer}>
+        <OdeContent />
+      </section>
     </Layout>
   );
 }
