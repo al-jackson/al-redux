3bd775e10841f66f629c94731b14adec5812ec23
Jamie Carter <jc2820@hotmail.com>
2021-04-16 20:39:05 +0100

    responsive bg on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,23 @@
 import Layout from "../components/layout";
-import styles from "../styles/Home.module.css";
 import { attributes } from "../content/home.md";
 
 export default function Home() {
   let { HomeImage } = attributes;
   return (
     <Layout page={"Home"}>
-      <article>
-        <img
-          src={"/" + HomeImage}
-          alt="full screen homepage image"
-          className={styles.homeImage}
-        />
-      </article>
+      <div className="homeImage"></div>
+      <style jsx>{`
+        .homeImage {
+          background: url(/${HomeImage}) no-repeat center center fixed;
+          -webkit-background-size: cover;
+          -moz-background-size: cover;
+          -o-background-size: cover;
+          background-size: cover;
+          width: 100vw;
+          height: 100vh;
+          z-index: -1;
+        }
+      `}</style>
     </Layout>
   );
 }
