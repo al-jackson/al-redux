e6360e8671b864092097a2fc34e45abc155db64a
Jamie Carter <jc2820@hotmail.com>
2021-04-16 19:04:05 +0100

    started the contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,13 +1,22 @@
 import Layout from "../components/layout";
 import styles from "../styles/contact.module.css";
-// import Image from "next/image"
-// import { attributes } from "../content/home.md";
 
 export default function Contact() {
-  //   let { HomeImage } = attributes;
   return (
     <Layout page={"Contact"}>
       <div className={styles.background}></div>
+      <section className={styles.contactContainer}>
+        <div className={styles.contactType}>
+          <img src="/img/gmail-icon.png" alt="gmail icon" className={styles.gmailIcon} />
+          <p>alpauljackson@gmail.com</p>
+        </div>
+        <div className={styles.contactType}>
+        <img src="/img/insta-icon.png" alt="instagram icon" className={styles.instaIcon} />
+          <a href="https://instagram.com/al_redux/" alt="al's insta">
+            al_redux
+          </a>
+        </div>
+      </section>
     </Layout>
   );
 }
