0589908847fe3108db6ac097061a8a6ecc621a0f
Jamie Carter <jc2820@hotmail.com>
2021-04-21 18:30:06 +0100

    add videos to video page

--- pages/video.js
+++ pages/video.js
@@ -1,16 +1,27 @@
 import Layout from "../components/layout";
-import styles from "../styles/video.module.css"
-// import Image from "next/image"
-// import { attributes } from "../content/home.md";
-
+import styles from "../styles/video.module.css";
+import { attributes } from "../content/video.md";
 
 export default function Video() {
-//   let { HomeImage } = attributes;
+  let { videoPost } = attributes;
   return (
     <Layout page={"Video"}>
-      <div className={styles.background}>
-
-      </div> 
+      <div className={styles.background}></div>
+      {videoPost.map((video, k) => {
+        return (
+          <div className={styles.videoContainer} key={k}>
+              <iframe
+                width="853"
+                height="480"
+                src={`https://www.youtube.com/embed/${video.url}`}
+                title={video.title}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+          </div>
+        );
+      })}
     </Layout>
   );
 }
