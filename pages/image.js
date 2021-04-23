9e999ace0f87b6a94fe96f26b07abe2a9a06ea82
Jamie Carter <jc2820@hotmail.com>
2021-04-23 11:08:49 +0100

    image page updates

--- pages/image.js
+++ pages/image.js
@@ -5,18 +5,21 @@ export default function Image() {
   let { imagePost } = attributes;
   return (
     <Layout page={"Image"}>
+      <div className="background"></div>
       {imagePost
         ? imagePost.map((post, k) => {
             return post.fullscreen ? (
               <div className="post" key={k}>
+                <div className="post-image-container">
                 <div
                   style={{
                     background: `url(/${post.image}) no-repeat center center`,
                     backgroundSize: "cover",
                     width: "100vw",
-                    height: "90vh",
+                    height: "80vh",
                   }}
                 ></div>
+                </div>
                 <div className="post-title-container">
                   <h2 className="post-title">{post.title}</h2>
                 </div>
@@ -28,7 +31,7 @@ export default function Image() {
                     background: `url(/${post.image}) no-repeat center center`,
                     backgroundSize: "contain",
                     width: "100vw",
-                    height: "90vh",
+                    height: "80vh",
                   }}
                 ></div>
                 <div className="post-title-container">
@@ -40,29 +43,41 @@ export default function Image() {
         : null}
 
       <style jsx>{`
+        .background {
+          width: 100vw;
+          height: 100vh;
+          position: fixed;
+          top: 0;
+          left: 0;
+          background-color: white;          
+        }
         .post {
+          width: 100vw;
+          height: 100vh;
           display: flex;
           flex-direction: column;
+          justify-content: flex-end;
           position: relative;
           border: none;
           margin: 0;
           padding: 0;
         }
+        .post-image-container {
+          display: flex;
+          flex-direction: column;
+          justify-content: flex-end;
+          width: 100vw;
+          height: 90vh;
+        }
         .post-title-container {
-          background-color: rgba(148, 148, 148, 1);
+          background-color: white;
           width: 100vw;
           height: 10vh;
           display: flex;
           align-items: center;
           padding-left: 25px;
-          color: white;
+          color: rgba(148, 148, 148, 1);
           z-index: 0;
-        }
-        .non-full-image {
-          width: auto;
-          height: auto;
-          max-width: 100vw;
-          max-height: 100vh;
         }
       `}</style>
     </Layout>
