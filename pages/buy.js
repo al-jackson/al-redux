e2f23d9c308fc78179b4ec0e1b65a9471a0513e4
Jamie Carter <jc2820@hotmail.com>
2021-04-28 12:32:43 +0100

    more css nonsense

--- pages/buy.js
+++ pages/buy.js
@@ -4,13 +4,17 @@ import styles from "../styles/buy.module.css";
 export default function Buy() {
   return (
     <Layout page={"Buy"}>
-      <div className={styles.background}>
-        <section className={styles.buyContainer}>
+      <div className={styles.background}></div>
+      <section className={styles.buyContainer}>
+        <div>
           <p>Sometimes I produce one-off runs of my work in various forms,</p>
           <p>please check the link below for available items,</p>
-          <a href="#" className={styles.buyLink}>shop</a>
-        </section>
-      </div>
+          <br />
+          <a href="#" className={styles.buyLink}>
+            shop
+          </a>
+        </div>
+      </section>
     </Layout>
   );
 }
